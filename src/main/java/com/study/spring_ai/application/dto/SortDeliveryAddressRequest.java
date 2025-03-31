46215963c3c4dc1d844b451ea0377d037eba2b35
devdaeun <ekdms008@gmail.com>
2025-03-31 10:57:43 +0900

    배송지 정렬 요청 객체 정의 [X]도착지 허브 주소와 정렬이 필요한 배송지 목록을 필드로 가지는 record 객체 정의 [X]SortDeliveryAddressRequest.java

---
+++ src/main/java/com/study/spring_ai/application/dto/SortDeliveryAddressRequest.java
@@ -0,0 +1,9 @@
+package com.study.spring_ai.application.dto;
+
+import java.util.List;
+
+public record SortDeliveryAddressRequest(
+        String hubAddress,
+        List<?> deliveryAddress
+) {
+}
