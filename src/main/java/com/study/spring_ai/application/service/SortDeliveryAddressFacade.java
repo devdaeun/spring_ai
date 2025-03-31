7f47961bdf222613fc2113ed1393632ca001143e
devdaeun <ekdms008@gmail.com>
2025-03-31 11:33:42 +0900

    배송지 목록 정렬 Service 구현: SortDeliveryAddressFacade [X]DIP 원칙에 따라 고수준 모듈이 ChatGPT와 통신하는 저수준 모듈의 구체적인 구현체에 직접 의존하지 않도록 인터페이스를 이용하여 의존 관계를 설정 [X]인터페이스를 통해 고수준 모듈에서 저수준 모듈의 구현체를 교체하거나 확장할 수 있도록 구성 [X]고수준 모듈: SortDeliveryAddressFacade.java [X]저수준 모듈: SortDeliveryAddressChatService.java [X]인터페이스: SortDeliveryAddressService.java

---
+++ src/main/java/com/study/spring_ai/application/service/SortDeliveryAddressFacade.java
@@ -0,0 +1,21 @@
+package com.study.spring_ai.application.service;
+
+import com.study.spring_ai.application.dto.SortDeliveryAddressRequest;
+import com.study.spring_ai.infastructure.dto.SequencedDeliveryAddress;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SortDeliveryAddressFacade {
+    private final SortDeliveryAddressService sortDeliveryAddressService;
+
+    public List<SequencedDeliveryAddress> sortDeliveryAddress(SortDeliveryAddressRequest request) {
+        return sortDeliveryAddressService.sortDeliveryAddress(
+                request.hubAddress(),
+                DeliveryAddressFormatter.format(request.deliveryAddress())
+        );
+    }
+}
--- src/main/java/com/study/spring_ai/infastructure/SortDevliveryAddressChatService.java
+++ src/main/java/com/study/spring_ai/infastructure/SortDevliveryAddressChatService.java
@@ -1,6 +1,7 @@
 package com.study.spring_ai.infastructure;
 
-import com.study.spring_ai.application.service.SortDeliveryAddressChatService;
+import com.study.spring_ai.application.service.SortDeliveryAddressService;
+import com.study.spring_ai.infastructure.dto.SequencedDeliveryAddress;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,12 +11,12 @@ import java.util.List;
 import static com.study.spring_ai.infastructure.SortDeliveryPromptTemplate.promptTemplate;
 
 @RequiredArgsConstructor
-public class SortDevliveryAddressChatService implements SortDeliveryAddressChatService{
+public class SortDevliveryAddressChatService implements SortDeliveryAddressService {
 
     private final ChatClient chatClient;
 
     @Override
-    public List<?> sortDeliveryAddressChat(String hubAddress, String formattedAddress) {
+    public List<SequencedDeliveryAddress> sortDeliveryAddress(String hubAddress, String formattedAddress) {
         return chatClient.prompt()
                 .user(userSpec->userSpec.text(promptTemplate)
                         .param("hub_address", hubAddress)
